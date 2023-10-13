39c5195f5723d4a33624597f9700579bb66f9688
rakeshkandhi <rakeshkandhi1432@gmail.com>
2023-10-13 14:59:27 +0530

    updated footer

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
-def home_page():
+def homePage():
     return render_template('home.html')
 
 @app.route("/prediction")
 def predictionPage():
     return render_template('predict.html')
-@app.route("/about")
-def aboutPage():
-    return render_template('about.html')
+
